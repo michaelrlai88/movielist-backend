348b36531ff1103bac3c5c1723a10c9521f71d19
michaelrlai88 <michaelrlai88@gmail.com>
2021-08-04 00:40:38 -0700

    fix cors

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const cors = require("cors");
+const cors = require('cors');
 
-const auth = require("./routes/auth");
-const api = require("./routes/api");
+const auth = require('./routes/auth');
+const api = require('./routes/api');
+
+const corsOptions = {
+  origin: ['https://movielist88.vercel.app', 'http://http://localhost:3000'],
+};
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
-app.use("/auth", auth);
-app.use("/api/v1", api);
+app.use('/auth', auth);
+app.use('/api/v1', api);
 
 const port = process.env.PORT || 5001;
 

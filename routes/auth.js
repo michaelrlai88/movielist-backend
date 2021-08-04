234813af481f3166e1ab794519aff3d4e9c874a9
michaelrlai88 <michaelrlai88@gmail.com>
2021-08-04 01:34:27 -0700

    fix fors

--- routes/auth.js
+++ routes/auth.js
@@ -16,6 +16,8 @@ router.post(
     .isLength({ min: 3 })
     .withMessage('Please enter a valid password'),
   async (req, res) => {
+    res.status(200).json('success');
+
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -65,8 +67,6 @@ router.post(
 
 //Login endpoint
 router.post('/login', async (req, res) => {
-  res.status(200).json('success');
-
   try {
     const { email, password } = req.body;
 
